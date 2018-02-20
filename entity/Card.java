7ba0be641718e2b53554529b7689a0ce3380c58d
Richard_Wellianto <13514051@std.stei.itb.ac.id>
2018-02-20 15:17:40 +0700

    Fix Persistence, Finish Branch & Order

--- entity/Card.java
+++ entity/Card.java
@@ -15,74 +15,79 @@ import javax.persistence.*;
  */
 @Entity
 public class Card implements Serializable {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
+  
+  public enum Type {
+    credit,
+    debit;
+  }
+  
+  @Id
+  private String number;
+  
+  private String bank;
+  private Type type;
+  
+  @Temporal(javax.persistence.TemporalType.DATE)
+  private Date lastTransactionDate;
+  private int totalTransaction;
     
-    public enum Type {
-        credit,
-        debit;
-    }
+  @Temporal(javax.persistence.TemporalType.DATE)
+  private Date expirationDate;
+  
+  //TODO relationship set<Order> paidOrder inverse Order::usedCard; + SETTER + GETTER
+  //private HashSet<Order> paidOrder;
     
-    @Id
-    private String number;
-    
-    private String bank;
-    private Type type;
-    
-    @Temporal(javax.persistence.TemporalType.DATE)
-    private Date lastTransactionDate;
-    private int totalTransaction;
-    
-    @Temporal(javax.persistence.TemporalType.DATE)
-    private Date expirationDate;
-        
-    public Card(){
-    }
-    
-    public Card(String number){
-        this.number = number;
-    }
-    
-    public String getNumber(){
-        return number;
-    }
-    
-    public String getBank(){
-        return bank;
-    }
-    
-    public Type getType(){
-        return type;
-    }
-    
-    public Date getLastTransactionDate(){
-        return lastTransactionDate;
-    }
-    
-    public int getTotalTransaction(){
-        return totalTransaction;
-    }
-    
-    public Date getExpirationDate(){
-        return expirationDate;
-    }
-    
-    public void setBank(String bank){
-        this.bank = bank;
-    }
-    
-    public void setType(Type type){
-        this.type = type;
-    }
-    
-    public void setLastTransactionDate(Date lastTransactionDate){
-        this.lastTransactionDate = lastTransactionDate;
-    }
-    
-    public void setTotalTransaction(int totalTransaction){
-        this.totalTransaction = totalTransaction;
-    }
-    
-    public void setExpirationDate(Date expirationDate){
-        this.expirationDate = expirationDate;
-    }
+  public Card(){
+  
+  }
+  
+  public Card(String number){
+    this.number = number;
+  }
+  
+  public String getNumber(){
+    return number;
+  }
+  
+  public void setBank(String bank){
+    this.bank = bank;
+  }
+  
+  public String getBank(){
+    return bank;
+  }
+  
+  public void setType(Type type){
+    this.type = type;
+  }
+  
+  public Type getType(){
+    return type;
+  }
+  
+  public void setLastTransactionDate(Date lastTransactionDate){
+    this.lastTransactionDate = lastTransactionDate;
+  }
+  
+  public Date getLastTransactionDate(){
+    return lastTransactionDate;
+  }
+  
+  public void setTotalTransaction(int totalTransaction){
+    this.totalTransaction = totalTransaction;
+  }
+  
+  public int getTotalTransaction(){
+    return totalTransaction;
+  }
+  
+  public void setExpirationDate(Date expirationDate){
+    this.expirationDate = expirationDate;
+  }
+  
+  public Date getExpirationDate(){
+    return expirationDate;
+  }
+  
 }
--- entity/Employee.java
+++ entity/Employee.java
@@ -16,109 +16,133 @@ import javax.persistence.*;
  */
 @Entity
 public class Employee implements Serializable{
-    private static final long serialVersionUID = 1L;
-    
-    @Id
-    private String id;
-    
-    private String name;
-    
-    @OneToOne
-    private Location location;
-    private HashSet<String> phoneNumber;
-    
-    @Temporal(javax.persistence.TemporalType.DATE)
-    private Date startingDate;
-    private int baseSalary;
-    
-    @Temporal(javax.persistence.TemporalType.DATE)
-    private Date birthDate;
-    
-    public int getAge() {
-        long ageMs = new Date().getTime()-birthDate.getTime();
-        Date age = new Date(ageMs);
-        return age.getYear();
-    };
-    
-    //TODO relationship Branch workplace inverse Branch::worker; + SETTER + GETTER
-    //private Branch workplace;
-    
-    //TODO relationship set<Order> handledOrder inverse Order::handler; + SETTER + GETTER
-    //private HashSet<Order> handledOrder;
-    
-    //TODO set<ExecutiveCustomer> consultedCustomer inverse ExecutiveCustomer::consultant; + SETTER + GETTER
-    //private HashSet<ExecutiveCustomer> consultedCustomer;
-
-    public Employee(){
-        this.phoneNumber = new HashSet<>();
-        //this.handledOrder = new HashSet<>();
-        //this.consultedCustomer = new HashSet<>();
-    }
-    
-    public Employee(String id){
-        this.phoneNumber = new HashSet<>();
-        this.id = id;
-        //this.handledOrder = new HashSet<>();
-        //this.consultedCustomer = new HashSet<>();
-    }
-    
-    public String getID(){
-        return id;
-    }
-    
-    public String getName(){
-        return name;
-    }
-    
-    public Location getLocation(){
-        return location;
-    }
-    
-    public HashSet<String> getPhoneNumber() {
-        return phoneNumber;
-    }
-    
-    public Date getStartingDate(){
-        return startingDate;
-    }
-    
-    public int getBaseSalary(){
-        return baseSalary;
-    }
-    
-    public Date getBirthDate(){
-        return birthDate;
-    }
-    
-    public void setName(String name){
-        this.name = name;
-    }
-    
-    public void setLocation(Location location){
-        this.location = location;
-    }
-    
-    public void setPhoneNumber(HashSet<String> phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+  private static final long serialVersionUID = 1L;
   
-    public void addPhoneNumber(String phoneNumber) {
-        this.phoneNumber.add(phoneNumber);
-    }
-    public void delPhoneNumber(String phoneNumber) {
-        this.phoneNumber.remove(phoneNumber);
-    }
+  @Id
+  private String id;
+  
+  private String name;
+  
+  @OneToOne
+  private Location location;
+  private HashSet<String> phoneNumber;
+  
+  @Temporal(javax.persistence.TemporalType.DATE)
+  private Date startingDate;
+  private int baseSalary;
+  
+  @Temporal(javax.persistence.TemporalType.DATE)
+  private Date birthDate;
+  
+  public int getAge() {
+    long ageMs = new Date().getTime()-birthDate.getTime();
+    Date age = new Date(ageMs);
+    return age.getYear();
+  };
+  
+  @ManyToOne
+  private Branch workplace;
+  
+  //TODO relationship set<Order> handledOrder inverse Order::handler; + SETTER + GETTER
+  //private HashSet<Order> handledOrder;
+  
+  @OneToMany
+  private HashSet<ExecutiveCustomer> consultedCustomer;
+  
+  public Employee(){
+    this.phoneNumber = new HashSet<>();
+    //this.handledOrder = new HashSet<>();
+    this.consultedCustomer = new HashSet<>();
+  }
+  
+  public Employee(String id){
+    this.phoneNumber = new HashSet<>();
+    this.id = id;
+    //this.handledOrder = new HashSet<>();
+    this.consultedCustomer = new HashSet<>();
+  }
+  
+  public String getID(){
+    return id;
+  }
+  
+  public void setName(String name){
+    this.name = name;
+  }
+  
+  public String getName(){
+    return name;
+  }
+  
+  public void setLocation(Location location){
+    this.location = location;
+  }
+  
+  public Location getLocation(){
+    return location;
+  }
+  
+  public void setPhoneNumber(HashSet<String> phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+  
+  public void addPhoneNumber(String phoneNumber) {
+    this.phoneNumber.add(phoneNumber);
+  }
+  public void delPhoneNumber(String phoneNumber) {
+    this.phoneNumber.remove(phoneNumber);
+  }
+  
+  public HashSet<String> getPhoneNumber() {
+    return phoneNumber;
+  }
+  
+  public void setStartingDate(Date startingDate){
+    this.startingDate = startingDate;
+  }
+  
+  public Date getStartingDate(){
+    return startingDate;
+  }
+  
+  public void setBaseSalary(int baseSalary){
+    this.baseSalary = baseSalary;
+  }
+  
+  public int getBaseSalary(){
+    return baseSalary;
+  }
+  
+  public void setBirthDate(Date birthDate){
+    this.birthDate = birthDate;
+  }
+  
+  public Date getBirthDate(){
+    return birthDate;
+  }
+  
+  public void setBranch(Branch workplace){
+    this.workplace = workplace;
+  }
     
-    public void setStartingDate(Date startingDate){
-        this.startingDate = startingDate;
-    }
-    
-    public void setBaseSalary(int baseSalary){
-        this.baseSalary = baseSalary;
-    }
-    
-    public void setBirthDate(Date birthDate){
-        this.birthDate = birthDate;
-    }
+  public Branch getBranch(){
+    return workplace;
+  }
+  
+  public void setConsultedCustomer(HashSet<ExecutiveCustomer> consultedCustomer){
+    this.consultedCustomer = consultedCustomer;
+  }
+  
+  public void addConsultedCustomer(ExecutiveCustomer consultedCustomer){
+    this.consultedCustomer.add(consultedCustomer);
+  }
+  
+  public void delConsultedCustomer(ExecutiveCustomer consultedCustomer){
+    this.consultedCustomer.remove(consultedCustomer);
+  }
+  
+  public HashSet<ExecutiveCustomer> getConsultedCustomer(){
+    return consultedCustomer;
+  }
         
 }
